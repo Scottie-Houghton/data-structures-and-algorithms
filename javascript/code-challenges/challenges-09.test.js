31f781c01fae7dc1ed685dc09e0074b59ab38c9d
Scottie <scott.c.houghton@gmail.com>
2022-06-24 09:17:52 -0700

    Passed all Challenges

--- javascript/code-challenges/challenges-09.test.js
+++ javascript/code-challenges/challenges-09.test.js
@@ -121,7 +121,9 @@ const characters = [
 
 const getHouses = (arr) => {
   let houses = [];
-  // Solution code here...
+  arr.forEach(person => {
+    houses.push(person.house);
+  });
   return houses;
 };
 
@@ -138,8 +140,15 @@ hasChildrenValues(characters, 'Sansa') will return false
 ------------------------------------------------------------------------------------------------ */
 
 const hasChildrenValues = (arr, character) => {
-  // Solution code here...
-
+  let children = 0;
+  arr.forEach(person => {
+    if(person.name === character){
+      Object.keys(person).forEach((key,idx) => {
+        if(key === 'children') children = Object.values(person)[idx].length;
+      });
+    }
+  });
+  return children ? true : false;
 };
 
 /* ------------------------------------------------------------------------------------------------
